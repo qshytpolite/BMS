2d08cdc579fd4fedac2c7b606f11df0ac6455c74
Wachanga <31168616+qshytpolite@users.noreply.github.com>
2024-11-23 05:44:53 +0300

    Add files via upload

---
+++ BMS/app/models.py
@@ -0,0 +1,22 @@
+# Define database models
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+    id = Column(Integer, primary_key=True, index=True)
+    category = Column(String, index=True)
+    amount = Column(Float)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User")
